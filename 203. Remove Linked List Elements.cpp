bf96cf39286bbe719cc903c546d7444fab205ab3
WanPengQAQ <164981154@qq.com>
2017-05-14 13:18:37 +0800

    Update 203. Remove Linked List Elements.cpp

--- 203. Remove Linked List Elements.cpp
+++ 203. Remove Linked List Elements.cpp
@@ -31,3 +31,15 @@ public:
         return tempHead.next;
     }
 };
+
+//a better Solution
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL) return NULL;
+        
+        head->next = removeElements(head->next, val);
+        
+        return head->val == val ? head->next : head;
+    }
+};
